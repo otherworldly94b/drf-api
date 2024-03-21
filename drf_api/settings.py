1c97db5d280dacfddd9ed82761d52730013e622a
otherworldly94b <and.charalambous@gmail.com>
2024-03-21 18:49:04 +0000

    add profile serializer and list view

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-6wb#ip0z2!i%o*pqxqwu4wmwrn)l_y!dnzo_k=#f*@%ttv2e!o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-otherworldly94b-drfapi-t70rzsubq8p.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'rest_framework',
 
     'profiles',
 ]
