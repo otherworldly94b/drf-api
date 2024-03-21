a9d3f4f46d915238d356a694c1aafd401b1aa3ab
otherworldly94b <and.charalambous@gmail.com>
2024-03-21 19:08:39 +0000

    add profiles detail view

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-6wb#ip0z2!i%o*pqxqwu4wmwrn)l_y!dnzo_k=#f*@%ttv2e!o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-otherworldly94b-drfapi-t70rzsubq8p.ws-eu110.gitpod.io']
 
 
 # Application definition
