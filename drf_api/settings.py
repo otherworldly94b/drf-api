02db9977fc2e5d9df89aa66fdf10fc037376b8dc
otherworldly94b <and.charalambous@gmail.com>
2024-03-27 20:14:59 +0000

    correct heroku app link

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -60,7 +60,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-otherworldly94b-drfapi-t70rzsubq8p.ws-eu110.gitpod.io', 'drf-api-ach.herokuapp.com']
+ALLOWED_HOSTS = ['8000-otherworldly94b-drfapi-t70rzsubq8p.ws-eu110.gitpod.io', 'drf-api-ach-34946935b146.herokuapp.com']
 
 
 # Application definition
